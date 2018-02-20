b424e70619add435ef1a12a38cf3cc67f5db2201
Darren Poon <dyhpoon@gmail.com>
2018-02-20 12:09:10 +0800

    fix regex

--- test/passport-auth0.test.js
+++ test/passport-auth0.test.js
@@ -24,23 +24,24 @@ describe('test/passport-auth0.test.js', () => {
   });
 
   it('should GET /passport/auth0 redirect to auth url', async () => {
-    const { domain, clientID, callbackURL } = app.config.passportAuth0;
-    const regex = new RegExp(`^https://${domain}/authorize?response_type=code&redirect_uri=${encodeURI(callbackURL)}&client_id=${clientID}`);
-    // https://onion-sample.auth0.com/authorize?response_type=code&redirect_uri=http%3A%2F%2F127.0.0.1%3A53485%2Fpassport%2Fauth0%2Fcallback&client_id=6yI6fwldg459UqkI6DPkvuRcRBMEfmnW
+    const { domain, clientID } = app.config.passportAuth0;
+    const regex = new RegExp(`^https:\/\/${domain}\/authorize\\?response_type=code&redirect_uri=http(.*)&client_id=${clientID}`);
 
     const res = await request(app.callback())
       .get('/passport/auth0')
       .expect(302);
 
-    console.log(callbackURL);
-    assert(callbackURL === 2);
     assert(regex.test(res.header.location));
   });
 
-  it('should GET /passport/auth0/callback redirect to auth url', () => {
-    return request(app.callback())
+  it('should GET /passport/auth0/callback redirect to auth url', async () => {
+    const { domain, clientID } = app.config.passportAuth0;
+    const regex = new RegExp(`^https:\/\/${domain}\/authorize\\?response_type=code&redirect_uri=http(.*)&client_id=${clientID}`);
+
+    const res = await request(app.callback())
       .get('/passport/auth0/callback')
-      .expect('Location', /^https:\/\/github.com\/login\/oauth\/authorize\?response_type=code&redirect_uri=http/)
       .expect(302);
+
+    assert(regex.test(res.header.location));
   });
 });
